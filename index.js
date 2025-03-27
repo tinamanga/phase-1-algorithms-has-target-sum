28134e03a2fdabbfd5e6fd9b8972e9ebf55e33ff
tinamanga <christinamanga28@gmail.com>
2025-03-28 01:31:03 +0300

    practicing alogarithm problem solving and identifying the Big O of an alogarithm

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function hasTargetSum(array, target) {
+function hasTargetSum(arr, target) {
   // Write your algorithm here
+  const seen = new Set(); // This will store the numbers we've encountered so far
+    
+    for (let num of arr) {
+        const complement = target - num; // Find the complement that would add up to the target
+        
+        if (seen.has(complement)) {  // Check if we've already seen the complement
+            return true;  // If we have, return true because we found a pair
+        }
+        
+        seen.add(num); // Otherwise, add the current number to the set
+    }
+    
+    return false;  // If we fini
 }
 
 /* 
@@ -8,6 +21,12 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // true (3 + 7 = 10)
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));  // true (19 + 6 = 25)
+console.log(hasTargetSum([1, 2, 5], 4));            // false (no pair sums to 4)
+console.log(hasTargetSum([1, 3, 5, 7], 8));         // true (3 + 5 = 8)
+console.log(hasTargetSum([1, 1, 1, 1], 2));         // true (1 + 1 = 2)
+
 */
 
 /*
